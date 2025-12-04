b77616ce8fb23cc6f30719dce8c7c34913b5a9b5
yuu0w0-1009 <5322brian@gmail.com>
2025-12-04 23:09:00 +0800

    12/4

---
+++ hw7.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(void){
+    int input = 0, chose = 0;
+    int change = 0, get = 0;
+    int drink[6] = {0,15,20,25,30,50};
+
+    while (1){
+        scanf("%d",&input);
+        if(input<0)break;
+        scanf("%d",&chose);
+
+    }
+    
+
+
+    system("pause");
+    return 0;
+}
